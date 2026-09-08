af6b3082e11fe55b5ef72d159d58f8ee7be76601
VladimirTsertseil <vladimir.tsertseil@gmail.com>
2026-09-08 18:31:38 +0300

    Update push-config.js

--- push-config.js
+++ push-config.js
@@ -1,4 +1,4 @@
 window.TRACKER_PUSH_CONFIG = {
-  BACKEND_URL: 'https://tracker116-push-backend.onrender.com',
+  BACKEND_URL: 'https://tracker116-push.vladimir-tsertseil.workers.dev',
   VAPID_PUBLIC_KEY: 'BJ0_VcOJaqa5AnDViccHyy6MRnuoUh3Z3Z_PM1sgGBAQbkyb5YFzvsxLl7GtHd7bh2hF0DLu_AHni5XK7TYBknc'
 };
